e9f861bfec19be341226c7cf630952b1f97b84dc
Cleison Nunes <cleison.nunes20@gmail.com>
2023-03-12 10:51:58 -0300

    Criado a classe posicao xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,21 +9,23 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            //try
+            //{
+            //    Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,9));
+            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
 
 
-                Tela.imprimirTabuleiro(tab);
-               
-            }
-            catch(TabuleiroExceptions ex)
-            { Console.WriteLine(ex.Message); }
-           
+            //    Tela.imprimirTabuleiro(tab);
+
+            //}
+            //catch(TabuleiroExceptions ex)
+            //{ Console.WriteLine(ex.Message); }
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos.ToPosicao());
+
             Console.WriteLine();
         }
     }
