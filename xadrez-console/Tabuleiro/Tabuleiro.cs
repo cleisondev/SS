460b4c5b7c587a0cf8e61c9d0022894aa8816f4e
Cleison Nunes <cleison.nunes20@gmail.com>
2023-03-12 10:01:59 -0300

    Tratando erros com exceção

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,5 +1,4 @@
 ﻿
-
 namespace tabuleiro
 {
      class Tabuleiro
@@ -15,15 +14,44 @@ namespace tabuleiro
             pecas = new Peca[linhas,colunas];
         }
 
+        public bool existePeca(Posicao pos) 
+        {
+            ValidarPosicao(pos);
+            return peca(pos) != null;     
+        }
+
         public Peca peca(int linha, int coluna) 
         {
             return pecas[linha,coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
         public void colocarPeca(Peca p, Posicao pos) 
         {
+            if(existePeca(pos)) { throw new TabuleiroExceptions("Já existe uma peça nessa posição"); }
+
             pecas[pos.linha,pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posValida(Posicao pos) 
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+         
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!posValida(pos))
+            {
+                throw new TabuleiroExceptions("Posição inválida");
+            }
         }
     }
 }
---
+++ xadrez-console/Tabuleiro/TabuleiroExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace tabuleiro
+{
+     class TabuleiroExceptions : Exception
+    {
+        public TabuleiroExceptions(string message) : base(message)
+        {
+        
+        }
+
+    }
+}
